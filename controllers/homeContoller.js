9f2d7ce38fd876e0d7d95a057efa2c9fc87aceab
Prashant Singh <prashantdineshsingh48@gmail.com>
2023-04-14 17:38:49 +0530

    CSS update, delete btn working

--- controllers/homeContoller.js
+++ controllers/homeContoller.js
@@ -1,31 +1,32 @@
-var contectList = [
-    {
-        name : "Amit",
-        phone : "1234567890"
-    },
-
-    {
-        name : "Rishikesh",
-        phone : "0987654321"
-    },
-
-    {
-        name : "Manku",
-        phone : "5432167890"
-    }
-]
+var contectList = []
 
 module.exports.home = function (req, res) {
     return res.render("home", {
         title : "Contact List",
-        contact_list : contectList
+        contact_list : contectList   // this use for FOR-LOOP in ejs to connect the ejs FOR-LOOP name and contactList are same
     });
 }
 
 module.exports.creatContact = function (req, res) {
-    console.log(req.body);
+    // console.log(req.body);
 
     contectList.push(req.body);
+
+    return res.redirect('back');
+}
+
+module.exports.deleteContact = function (req, res) {
+    // console.log(req.query);
+
+    let phoneNumber = req.query.phone;
+    // console.log(phoneNumber);
+
+    let contactIndex = contectList.findIndex(contact => contact.phone == phoneNumber);
+    // console.log(contactIndex);
+
+    if( contactIndex != 1) {
+        contectList.splice(contactIndex, 1);
+    }
 
     return res.redirect('back');
 }
--- routers/index.js
+++ routers/index.js
@@ -8,6 +8,9 @@ router.get('/', homecontroller.home);
 
 router.post('/create-contact', homecontroller.creatContact);
 
+// router.get('/delete-contact/:phone', homecontroller.deleteContact);  ==> for param value
+
+router.get('/delete-contact', homecontroller.deleteContact);  // ==> for query
 
 
 module.exports = router;  
